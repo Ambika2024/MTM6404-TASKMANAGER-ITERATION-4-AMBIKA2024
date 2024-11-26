0dd09d9c7ab6c8870073f35097d8aff9b4916757
Ambika2024 <145181223+Ambika2024@users.noreply.github.com>
2024-11-26 16:21:54 -0500

    Completes the iteration-2

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header-container">
+      <h1>Task Manager</h1>
+      <p>Your go-to app for task organization</p>
+    </header>
+  );
+};
+
+export default Header;
